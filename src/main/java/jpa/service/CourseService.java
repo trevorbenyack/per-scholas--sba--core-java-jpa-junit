2de2cfa804e324225faf279ae72d465ec95001ac
Trevor Benyack <trevorbenyack@gmail.com>
2021-05-05 23:55:23 -0400

    completes first draft of CourseService methods

--- src/main/java/jpa/service/CourseService.java
+++ src/main/java/jpa/service/CourseService.java
@@ -2,17 +2,40 @@ package jpa.service;
 
 import jpa.dao.CourseDAO;
 import jpa.entitymodels.Course;
+import jpa.util.EntityManagerService;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
-public class CourseService implements CourseDAO {
+public class CourseService extends EntityManagerService implements CourseDAO {
     @Override
     public List<Course> getAllCourses() {
-        return null;
+        List<Course> coursesList;
+        EntityManager entityManager = getEntityManager();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> criteriaQuery = criteriaBuilder.createQuery(Course.class);
+        Root<Course> root = criteriaQuery.from(Course.class);
+        criteriaQuery.select(root);
+
+        coursesList = entityManager.createQuery(criteriaQuery).getResultList();
+        entityManager.close();
+
+        return coursesList;
     }
 
     @Override
-    public Course GetCourseById(int number) {
-        return null;
+    public Course GetCourseById(int courseId) {
+
+        Course course;
+        EntityManager entityManager = getEntityManager();
+
+        course = entityManager.find(Course.class, courseId);
+
+        entityManager.close();
+        return course;
     }
 }
