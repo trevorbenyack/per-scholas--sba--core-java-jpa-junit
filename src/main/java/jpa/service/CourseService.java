1ae1d7f5d29417defb27a33d2b9556be798b8e8d
Trevor Benyack <trevorbenyack@gmail.com>
2021-05-10 13:04:14 -0400

    adds error handling in CourseService class

--- src/main/java/jpa/service/CourseService.java
+++ src/main/java/jpa/service/CourseService.java
@@ -5,6 +5,7 @@ import jpa.entitymodels.Course;
 import jpa.util.EntityManagerService;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -13,7 +14,7 @@ import java.util.List;
 public class CourseService extends EntityManagerService implements CourseDAO {
     @Override
     public List<Course> getAllCourses() {
-        List<Course> coursesList;
+        List<Course> coursesList = null;
         EntityManager entityManager = getEntityManager();
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
@@ -21,21 +22,33 @@ public class CourseService extends EntityManagerService implements CourseDAO {
         Root<Course> root = criteriaQuery.from(Course.class);
         criteriaQuery.select(root);
 
-        coursesList = entityManager.createQuery(criteriaQuery).getResultList();
-        entityManager.close();
+        try {
+            coursesList = entityManager.createQuery(criteriaQuery).getResultList();
+        } catch (IllegalArgumentException | PersistenceException e) {
+            System.out.println("There was an error trying to retrieve the list of courses.");
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
 
         return coursesList;
-    }
+    } // end getAllCourses() method
 
     @Override
-    public Course GetCourseById(int courseId) {
+    public Course getCourseById(int courseId) {
 
-        Course course;
+        Course course = null;
         EntityManager entityManager = getEntityManager();
 
-        course = entityManager.find(Course.class, courseId);
+        try {
+            course = entityManager.find(Course.class, courseId);
+        } catch (IllegalArgumentException e) {
+            System.out.println("There was an error trying to find that course");
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
 
-        entityManager.close();
         return course;
-    }
+    } // end getCourseById() method
 }
