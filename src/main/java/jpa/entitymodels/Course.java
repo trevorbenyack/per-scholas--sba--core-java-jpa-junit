1cac0d086dc0895ff9b1d2c2f45fd7869a57fd1b
Trevor Benyack <trevorbenyack@gmail.com>
2021-05-10 13:01:49 -0400

    changes variable names to match requirements

--- src/main/java/jpa/entitymodels/Course.java
+++ src/main/java/jpa/entitymodels/Course.java
@@ -2,11 +2,9 @@ package jpa.entitymodels;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,15 +13,16 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Log4j
 public class Course {
 
-    @Id
-    int courseId;
+    @Id @Column(name = "id")
+    int cId;
 
-    String courseName;
+    @Column(name = "name")
+    String cName;
 
-    String courseInstructor;
+    @Column(name = "instructor")
+    String cInstructorName;
 
     @Override
     public boolean equals(Object o) {
@@ -32,16 +31,16 @@ public class Course {
 
         Course course = (Course) o;
 
-        if (courseId != course.courseId) return false;
-        if (!courseName.equals(course.courseName)) return false;
-        return courseInstructor.equals(course.courseInstructor);
+        if (cId != course.cId) return false;
+        if (!cName.equals(course.cName)) return false;
+        return cInstructorName.equals(course.cInstructorName);
     }
 
     @Override
     public int hashCode() {
-        int result = courseId;
-        result = 31 * result + courseName.hashCode();
-        result = 31 * result + courseInstructor.hashCode();
+        int result = cId;
+        result = 31 * result + cName.hashCode();
+        result = 31 * result + cInstructorName.hashCode();
         return result;
     }
 }
