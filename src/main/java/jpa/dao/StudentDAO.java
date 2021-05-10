bfb440d37279967132bb218d0b6cbbfcacc351eb
Trevor Benyack <trevorbenyack@gmail.com>
2021-05-10 13:09:13 -0400

    changes variable names to match requirements

--- src/main/java/jpa/dao/StudentDAO.java
+++ src/main/java/jpa/dao/StudentDAO.java
@@ -3,13 +3,15 @@ package jpa.dao;
 import jpa.entitymodels.Course;
 import jpa.entitymodels.Student;
 
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 public interface StudentDAO {
 
     List<Student> getAllStudents();
-    Student getStudentByEmail(String sEmail);
-    boolean validateStudent(String sEmail, String sPassword);
-    List<Course> getStudentCourses(String sEmail);
+    Student getStudentByEmail(String studentEmail);
+    boolean validateStudent(String studentEmail, String enteredPassword);
+    List<Course> getStudentCourses(String studentEmail);
     void registerStudentToCourse(String studentEmail, Course course);
 }
